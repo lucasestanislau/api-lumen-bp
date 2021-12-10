4decbae3fccf5f8fbab6a4b09d502712280faa78
Lucas Vidoto <lucasvidoto17x@gmail.com>
2021-12-09 21:55:57 -0300

    feat: adicionado mensagens de validação

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -18,7 +18,25 @@ class Aluno extends Model
             'nome' => 'string|required|max:120',
             'email' => 'string|email|required|max:200',
             'cpf' => 'string|required|max:11',
-            'data_nascimento' => 'date_format:Y-m-d|required|format',
+            'data_nascimento' => 'date_format:Y-m-d|required',
+        ];
+    }
+
+    static function mensagensValidacao()
+    {
+        return [
+            'nome.string' => 'Tipo inválido',
+            'nome.required' => 'Nome é obrigatório',
+            'nome.max' => 'Máximo de 120 caracteres',
+            'email.string' => 'Tipo inválido',
+            'email.required' => 'Email é obrigatório',
+            'email.email' => 'Formato do e-mail é inválido',
+            'email.max' => 'Máximo de 200 caracteres',
+            'cpf.string' => 'Tipo inválido',
+            'cpf.required' => 'Cpf é obrigatório',
+            'cpf.max' => 'Máximo de 11 caracteres',
+            'data_nascimento.date_format' => 'Formato inválido',
+            'data_nascimento.required' => 'Data de nascimento é obrigatório',
         ];
     }
 }
