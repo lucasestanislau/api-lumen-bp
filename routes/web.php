6d824d85682cc6e561cbf09bac7820311ba52544
Lucas Vidoto <lucasvidoto17x@gmail.com>
2021-12-09 22:08:33 -0300

    feat: adicionado rotas para o recurso aluno

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->post('/', 'Alunos\CriarAlunoController@executar');
+$router->get('/{id}', 'Alunos\VerAlunoController@executar');
+$router->get('/', 'Alunos\ListarAlunosController@executar');
